92d09a6266e36a85a05f83d60e8ef0f5332a6885
Dakota States <dakota.states@gmail.com>
2020-03-14 23:11:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -9,4 +9,9 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   
   def change
   end
+  
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 end
